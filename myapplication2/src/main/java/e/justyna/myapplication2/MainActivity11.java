dc767384859a339de3efe2562506072df8811fba
Justyna87 <justyna.przezdziecka@gmail.com>
2018-11-10 14:54:11 +0100

    Ignore .idea

--- myapplication2/src/main/java/e/justyna/myapplication2/MainActivity11.java
+++ myapplication2/src/main/java/e/justyna/myapplication2/MainActivity11.java
@@ -2,12 +2,18 @@ package e.justyna.myapplication2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity11 extends AppCompatActivity {
+    private static final String[] Hours = new String[]{"1 hour", "6 hours", "12 hours", "24 hours", "48 hours", "7days"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main11);
+        setContentView(R.layout.activity_email);
+        AutoCompleteTextView editText = findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Hours );
+        editText.setAdapter(adapter);
     }
 }
